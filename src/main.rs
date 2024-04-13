1a83d5862dd236a72acbbf86bede427ae6ea4d9b
kirlu1 <ulnedhan@gmail.com>
2024-04-13 19:26:20 +0200

    tinkering

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod common;
 mod application;
 
-use windows::core::{IUnknown, Interface, BSTR, GUID, PCWSTR, VARIANT};
-use windows::Win32::System::Com::{CoCreateInstance, CoInitialize, GetErrorInfo, IDispatch, IErrorInfo, ITypeInfo, CLSCTX};
+use windows::core::{w, IUnknown, Interface, BSTR, GUID, HRESULT, PCWSTR, VARIANT};
+use windows::Win32::System::Com::{CoCreateInstance, CoInitializeEx, GetErrorInfo, IDispatch, CLSCTX, COINIT_APARTMENTTHREADED, DISPATCH_FLAGS, DISPPARAMS, EXCEPINFO};
 
-use anyhow::{bail, Context, Error, Result};
+use anyhow::{bail, Error, Result};
 
-use common::variant::{EvilVariant, TypedVariant, VariantError};
+use common::variant::{opt_out_arg, EvilVariant, TypedVariant, VariantError};
 use common::dispatch::{DispatchError, HasDispatch, Invocation};
 
-use application::Outlook;
+use application::{Folder, Outlook};
 
 use once_cell::sync::OnceCell;
 
@@ -20,49 +20,56 @@ const OBJECT_CONTEXT: CLSCTX = windows::Win32::System::Com::CLSCTX_LOCAL_SERVER;
 const LOCALE_USER_DEFAULT: u32 = 0x0400;
 
 fn main() -> Result<(), WinError> {
+    co_initialize();
+
     let outlook = Outlook::new()?;
-
-    
-
-    let folder_path = vec!["ulrik.h@tryg.no", "Inbox"];
-    let target_path = vec!["ulrik.h@tryg.no"];
-
-    let folder1_path = vec!["ulrik.h@tryg.no", "Testfolder"];
-    let folder2_path = vec!["ulrik.h@tryg.no", "Test 2"];
+    let folder_path = vec!["Ulrik.Hansen@student.uib.no", "Inbox"];
+    let target_path = vec!["Ulrik.Hansen@student.uib.no", "Inbox", "test 2"];
 
     let Some(inbox) = outlook.get_folder(folder_path)? else {
         return Ok(())    };
     let Some(target_folder) = outlook.get_folder(target_path)? else {
         return Ok(());    };
-    let Some(folder1) = outlook.get_folder(folder1_path)? else {
-        return Ok(());    };
-    let Some(folder2) = outlook.get_folder(folder2_path)? else {
-        return Ok(());    };
 
+    let TypedVariant::Dispatch(picked_folder) = outlook.session().call("PickFolder", Invocation::Method, None)? else {
+        panic!("pickfolder failed")
+    };
+
+
+    let TypedVariant::Bstr(storeID1) = picked_folder.prop("StoreID")? else {
+        panic!()
+    };
+
+    let TypedVariant::Bstr(entryID1) = picked_folder.prop("EntryID")? else {
+        panic!()
+    };
+
+    let TypedVariant::Bstr(storeID2) = target_folder.prop("StoreID")? else {
+        panic!()
+    };
+
+    let TypedVariant::Bstr(entryID2) = target_folder.prop("EntryID")? else {
+        panic!()
+    };
     
+    println!("{}", entryID1);
+
+    println!("{}", entryID2);
+
+
+    let args = DISPPARAMS {
+        cArgs : 2,
+        rgvarg : vec![VARIANT::from(storeID2), VARIANT::from(entryID2)].as_mut_ptr(),
+        ..Default::default()
+    };
+
+    let TypedVariant::Dispatch(test_from_id) = outlook.session().call("GetFolderFromID", Invocation::Method, Some(args))? else {
+        panic!("ID DIDNT WORK")
+    };
+
     let email = inbox.iter()?.next().unwrap();
 
-    let error_info = unsafe {
-        GetErrorInfo(0)
-    };
-    let result = email.move_to(folder1);
-
-    dbg!(result);
-
-    
-    match error_info {
-        Err(_) => (),
-        Ok(info) => unsafe {
-            println!(
-                "{:?}\n{:?}\n{:?}\n{:?}\n",
-                info.GetDescription().unwrap().to_string(),
-                info.GetHelpContext().unwrap().to_string(),
-                info.GetSource().unwrap().to_string(),
-                info.GetHelpFile().unwrap().to_string(),
-            )
-        }
-    };
-    
+    email.move_to(Folder(test_from_id))?;
     
 
     Ok(())
@@ -90,7 +97,7 @@ fn co_initialize() {
     CO_INITIALIZED.get_or_init(
         || {
             let Ok(_) = (unsafe {
-                CoInitialize(None).ok()
+                CoInitializeEx(None, COINIT_APARTMENTTHREADED).ok()
             }) else {
                 panic!("Failed to CoInitialize");
             };
